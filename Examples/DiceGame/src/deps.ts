dacd35cadea9e32c978ca7c15a10442c6b608d4e
NDH <ndhrones@gmail.com>
2024-03-07 06:38:52 -0500

    r

--- Examples/DiceGame/src/deps.ts
+++ Examples/DiceGame/src/deps.ts
@@ -3,8 +3,8 @@
 //    Framework Deps
 //=================================================
 //export * from '../../../Framework/mod.ts';
-export * from "https://raw.githubusercontent.com/nhrones/Surface/main/Framework/mod.ts?token=GHSAT0AAAAAACOFY32HSTFIJHRDYJI3P3HSZPJUSVA"
+export * from "https://raw.githubusercontent.com/nhrones/Surface/main/Framework/mod.ts";
 //=================================================
 //    Component Deps
 //=================================================
-export * from '../../../Components/mod.ts'
+//export * from '../../../Components/mod.ts'
