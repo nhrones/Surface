438b009d1b9ac3e896f07b922e82e33cc4136800
NDH <ndhrones@gmail.com>
2024-03-07 06:29:48 -0500

    Update deps.ts

--- Examples/DiceGame/src/deps.ts
+++ Examples/DiceGame/src/deps.ts
@@ -3,7 +3,7 @@
 //    Framework Deps
 //=================================================
 //export * from '../../../Framework/mod.ts';
-export * from "https://github.com/nhrones/Surface/blob/main/Framework/mod.ts"
+export * from "https://raw.githubusercontent.com/nhrones/Surface/main/Framework/mod.ts?token=GHSAT0AAAAAACOFY32HSTFIJHRDYJI3P3HSZPJUSVA"
 //=================================================
 //    Component Deps
 //=================================================
